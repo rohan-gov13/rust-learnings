b4748209e0dc8f0a8bd756aac28fa5969b69530c
rohan-gov13 <rohan.govekar13@gmail.com>
2024-08-04 14:26:16 +0530

    Deck with Cards created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = ["hearts", "spades", "diamonds"];
+        let values = ["ace", "two", "three"];
+
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card)
+            }
+        }
+        
+        Deck { cards }
+
+    }
+}
+ 
 fn main() {
-    println!("Helloo, world!");
+    let deck = Deck::new();
+    
+    println!("deck {:#?}", deck);
 }
